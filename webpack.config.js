f620cf744f7f7d89f9910ac94c8f925cdf08f389
Eric Ferrer-Vaughn <ericace@outlook.com>
2017-08-20 21:35:11 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
 const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
 module.exports = {
   entry: './client/index.js',
   output: {
@@ -11,4 +19,6 @@ module.exports = {
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
   }
+  plugins: [HtmlWebpackPluginConfig]
 }
+
